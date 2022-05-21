3d9523b1d7bc3f363ee8b5eac2bf63fcb8390078
vineetsemwal <vineetsemwal82@gmail.com>
2022-05-21 11:47:20 +0530

    all crud operations done

--- frontend/promise_fetchapi/index.js
+++ frontend/promise_fetchapi/index.js
@@ -20,7 +20,7 @@ function fetchAll() {
 /**
  * adds an employee int the server
  */
-function add() {
+function addEmployee() {
     let url = "http://localhost:3000/employees";
     let requestData = {
         "name": "ayush",
@@ -51,7 +51,61 @@ function add() {
 }
 
 
-//add();
+/**
+ * updates an employee with id 4 in  the server
+ */
+ function updateEmployee() {
+    const id=4; 
+    const url = "http://localhost:3000/employees/"+id;
+    const requestData = {
+        "name": "ayush sharma",
+        "age": 24
+    };
+    let requestDataText = JSON.stringify(requestData);
+
+    let requestInfo =
+    {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+
+        body: requestDataText
+    };
+
+
+    let responsePromise = fetch(url, requestInfo);
+    responsePromise.then(response => {
+        let promiseResult = response.json();
+        promiseResult.then(employee => {
+            console.log("employee updated", employee);
+        });
+    });
+
+}
+
+/**
+ * delete employee with id 4
+ */
+function deleteEmployee(){
+    const id=4; 
+    const url = "http://localhost:3000/employees/"+id;
+    
+    let requestInfo =    {  method: "DELETE"  };
+
+
+    let responsePromise = fetch(url, requestInfo);
+    responsePromise.then(response => {
+        console.log("employee deleted with id "+id);
+    });
+
+}
+
+
+//addEmployee();
 fetchAll();
+//updateEmployee();
+//deleteEmployee();
 console.log("bye");
 
