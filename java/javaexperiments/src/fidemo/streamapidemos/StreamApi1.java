73c794d09da99382de855abe35bc9c1da681f7aa
vineetsemwal <vineetsemwal82@gmail.com>
2022-05-21 18:23:19 +0530

    more streamapi deo + pdf

--- java/javaexperiments/src/fidemo/streamapidemos/StreamApi1.java
+++ java/javaexperiments/src/fidemo/streamapidemos/StreamApi1.java
@@ -6,34 +6,65 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class StreamApi1 {
 
     public static void main(String args[]) {
         List<Product> list = new ArrayList<>();
-        list.add(new Product("samsung galaxy", "phone", 20000));
-        list.add(new Product("samsung tv", "tv", 50000));
-        list.add(new Product("mototolla phone", "phone", 15000));
+        list.add(new Product("samsung galaxy", "phone", 2000));
+        list.add(new Product("samsung tv", "tv", 5000));
+        list.add(new Product("mototolla phone", "phone", 1500));
+        list.add(new Product("iphone", "phone", 7000));
 
-       StreamApi1 demo = new StreamApi1();
-       double total1=demo.calculateTotal1(list);
-       double total2=demo.calculateTotal2(list);
-        System.out.println("total1="+total1);
-        System.out.println("total2="+total2);
+        StreamApi1 demo = new StreamApi1();
+        double total1 = demo.calculateTotal1(list);
+        double total2 = demo.calculateTotal2(list);
+        double total3 = demo.calculateTotal3(list);
+        System.out.println("total1=" + total1);
+        System.out.println("total2=" + total2);
+        System.out.println("total3=" + total3);
     }
 
-    public double calculateTotal2(Collection<Product>products){
-      Optional<Double>optional= products.stream()
-                .filter(product->product.getType().equals("phone"))
-                .map(product->product.getPrice())
-                .reduce((price1,price2)->price1+price2);
-        if(!optional.isPresent()){
+    /**
+     * calculate totl prices of all phones
+     */
+    public double calculateTotal2(Collection<Product> products) {
+        Optional<Double> optional = products.stream()
+                .filter(product -> product.getType().equals("phone"))
+                .map(product -> product.getPrice())
+                .reduce((price1, price2) -> price1 + price2);
+        if (!optional.isPresent()) {
             return 0;
         }
-       double total= optional.get();
+        double total = optional.get();
         return total;
     }
 
+
+    /**
+     * calculate totl prices of all phones
+     */
+    public double calculateTotal3(Collection<Product> products) {
+        Stream<Product> stream1 = products.stream();
+        Predicate<Product> predicate = product -> product.getType().equals("phone");
+        Stream<Product> stream2 = stream1.filter(predicate);
+        Stream<Double> stream3 = stream2.map(product -> product.getPrice());
+        BinaryOperator<Double> operator = (price1, price2) -> price1 + price2;
+        Optional<Double> optional = stream3.reduce(operator);
+        if (!optional.isPresent()) {
+            return 0;
+        }
+        double total = optional.get();
+        return total;
+    }
+
+
+    /**
+     * calculate totl prices of all phones
+     */
     public double calculateTotal1(Collection<Product> products) {
         double total = 0;
         for (Product product : products) {
--- java/javaexperiments/src/fidemo/streamapidemos/StreamApiDemo3.java
+++ java/javaexperiments/src/fidemo/streamapidemos/StreamApiDemo3.java
@@ -1,8 +1,6 @@
 package fidemo.streamapidemos;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -13,11 +11,18 @@ public class StreamApiDemo3 {
         list.add("hi");
         list.add("hi");
         list.add("bye");
+        list.add("good");
         StreamApiDemo3 demo = new StreamApiDemo3();
         System.out.println("**print different elements");
         demo.printDifferentElements(list);
         System.out.println("***print length of all elements");
         demo.printLengthOfAllElements(list);
+        System.out.println("***print max length");
+        demo.printMaximumLength(list);
+        System.out.println("***print min length");
+        demo.printMinimumLength(list);
+        System.out.println("sort and print even elements ");
+        demo.sortListOfEvenElements(list);
     }
 
     /**
@@ -37,5 +42,46 @@ public class StreamApiDemo3 {
         Consumer<Integer> consumer = System.out::println;
         stream2.forEach(consumer);
     }
+
+    public void printMaximumLength(Collection<String> collection) {
+        Stream<String> stream1 = collection.stream();
+        Function<String, Integer> fun = (str) -> str.length();
+        Stream<Integer> stream2 = stream1.map(fun);
+        Comparator<Integer> comparator = Integer::compare;
+        Optional<Integer> optional = stream2.max(comparator);
+        if (optional.isPresent()) {
+            int maxLength = optional.get();
+            System.out.println(maxLength);
+        } else {
+            System.out.println("length is zero");
+        }
+
+    }
+
+
+    public void printMinimumLength(Collection<String> collection) {
+        Stream<String> stream1 = collection.stream();
+        Function<String, Integer> fun = (str) -> str.length();
+        Stream<Integer> stream2 = stream1.map(fun);
+        Comparator<Integer> comparator = Integer::compare;
+        Optional<Integer> optional = stream2.min(comparator);
+        if (optional.isPresent()) {
+            int minLength = optional.get();
+            System.out.println(minLength);
+        } else {
+            System.out.println("length is zero");
+        }
+
+    }
+
+    public void sortListOfEvenElements(List<String>list){
+       Stream<String>stream1= list.stream();
+       Stream<String>stream2=stream1.filter(str->(str.length() %2)==0)  ;
+       Comparator<String>comparator=String::compareTo;
+       Stream<String>stream3 =stream2.sorted(comparator);
+       stream3.forEach(System.out::println);
+
+    }
+
 
 }
